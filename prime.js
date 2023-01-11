57f69b99b36c97abe0aa6fb0cc96ac2db32585e9
Dixit kumar <dxtt22@gmail.com>
2023-01-11 23:23:37 +0530

    done a prime problem

---
+++ prime.js
@@ -0,0 +1,13 @@
+let number=89;
+let count=0;
+for(let i=0;i<number;i++){
+if(number%i==0){
+count++;
+}
+}
+if(count==2){
+console.log("It is a prime number");
+}
+else{
+console.log("It is not a prime number")
+}
